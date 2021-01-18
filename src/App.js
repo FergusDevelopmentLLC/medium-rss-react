45fa1f1f476765e6d23e1ebfbe29994dc940d8d6
Will Carter <will.carter@fergusllc.com>
2021-01-18 14:48:44 -0700

    add fetchPosts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useEffect, useState } from "react"
+import FeaturedPosts from './components/FeaturedPosts'
+import Parser from 'rss-parser'
 
 function App() {
+
+  const parser = new Parser()
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+
+    const fetchPosts = async () => {
+      let feed = await parser.parseURL('http://138.68.23.63:4050/medium')
+      
+      let blogPosts = feed.items.reduce((acc, item) => {
+        
+        let blogPost
+
+        if(item["content:encoded"].indexOf("<figure>") > -1) {
+          blogPost = {}
+          blogPost.title = item["title"]
+          blogPost.url = item["link"]
+          blogPost.pubDate = item["pubDate"]
+
+          const content = new DOMParser().parseFromString(item["content:encoded"], 'text/html')
+          let subtitle = content.querySelector('p').textContent
+          if(subtitle.length < 100) blogPost.subtitle = subtitle
+
+          let image = content.querySelector('figure').querySelector('img')
+          if(image) blogPost.imgUrl = image.src
+        }
+
+        if(blogPost) acc.push(blogPost)
+
+        return acc
+      }, [])
+
+      if(blogPosts.length > 4) setPosts(blogPosts.slice(0, 5))
+    }
+
+    fetchPosts()
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <main></main>
+      <aside className="left">
+      <FeaturedPosts posts={posts} />
+      </aside>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
