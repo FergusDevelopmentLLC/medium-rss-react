7d25f917fe9fe30979d23f61064bf3a54a83d36a
Will Carter <will.carter@fergusllc.com>
2021-01-18 17:44:02 -0700

    refactor

--- src/components/FeaturedPosts.js
+++ src/components/FeaturedPosts.js
@@ -5,22 +5,19 @@ const FeaturedPosts = ({
 }) => {
   return (
     <ul>
-      {
-        posts.map((post, i) => {
+      {posts.map((post, i) => {
 
-          const dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
-          const postDate = new Date(post.pubDate)
-          
-          return (
+        const postDate = new Date(post.pubDate)
+        
+        return (
           <li key={ i }>
             <h4><a href={ post.url }>{ post.title }</a></h4>
             <div className="blog-subtitle"><a href={ post.url }>{ post.subtitle }</a></div>
-            <div className="blog-date">{ postDate.toLocaleDateString("en-US", dateOptions) }</div>
-            <a href='https://will-carter.medium.com/community-mojo-b989517e3c7e'><img src={post.imgUrl} alt={post.title} /></a>
+            <div className="blog-date">{ postDate.toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' }) }</div>
+            <a href={ post.url }><img src={ post.imgUrl } alt={ post.title } /></a>
           </li>
-          )
-        })
-      }
+        )
+      })}
     </ul>
   )
 }
